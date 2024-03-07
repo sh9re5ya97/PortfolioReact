0b0fff1b2fffe559f7a6fdbd1e29beb1ff3e0fb3
DESKTOP-RLFEEKI\Dell <singalshreya97@gmail.com>
2024-03-08 01:37:19 +0530

    color transitioning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import { ReactP5Wrapper } from "@p5-wrapper/react";
-import sketch from './components/sketch/sketch'
+import SketchComp from './components/sketch/sketch'
 
 function App() {
   return (
     <div className="App">
       PFOLIOLIO
-    <ReactP5Wrapper sketch={sketch}/>
+    <SketchComp />
     </div>
   );
 }
--- src/components/sketch/sketch.js
+++ src/components/sketch/sketch.js
@@ -1,20 +1,38 @@
+import { ReactP5Wrapper } from "@p5-wrapper/react";
 
-
+  let count=0;
+  let dec=false;
   function sketch(p5){
     p5.setup=function() {
       // Your setup code goes here
       p5.createCanvas(400, 400);
       // Run the draw() function once per second (you will learn about the frameRate() function in a later lesson)
-      p5.frameRate(1);
+      //p5.frameRate(1);
       // TODO: Move the background() function below to the draw() function
       p5.background(200);
     }
     
     p5.draw=function() {
+      if(count>255)dec=true;
+      if(dec){
+        count--;
+        if(count==0)dec=false;
+      }
+      else{
+      count++;
+      }
+      console.log(count);
+      p5.background(count);
       // Your drawing code goes here
       // TODO: Set background color to a random gray value
     
     }
+
+    
   }
 
-  export default sketch;
+  export default function SketchComp(){
+    return (
+      <ReactP5Wrapper sketch={sketch}/>
+    );
+  };
